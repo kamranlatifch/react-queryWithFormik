6f4f65d20c01273fa504ffbd4323fd783d542c0d
kamranlatifch <kamranlatif98@gmail.com>
2024-06-07 15:11:03 +0500

    fix(routes): fix the routes and app.js router configurations

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-    import { ChakraProvider, Heading, Switch } from '@chakra-ui/react';
-    import './App.css';
-    import { QueryClient,QueryClientProvider } from 'react-query';
-    import Home from './Home/Home';
-    import { Route, Router, Routes } from 'react-router-dom';
-    function App() {
+      import { ChakraProvider, Heading, Switch } from '@chakra-ui/react';
+      import './App.css';
+      import { QueryClient,QueryClientProvider } from 'react-query';
+      import Home from './Home/Home';
+      // import { ReactQueryDevtools } from 'react-query-devtools';
+      import { Route, BrowserRouter, Routes } from 'react-router-dom';
+      function App() {
 
-      const queryClient=new QueryClient();
-      return (
-        <ChakraProvider>
-        <QueryClientProvider client={queryClient}>
-        {/* <Heading> Welcome To React Query Practice with with Formik and Yup
-        </Heading>
-        <Home/> */}
-        <Router>
-    <Switch>
+        const queryClient=new QueryClient();
+        return (
+          <ChakraProvider>
+            <QueryClientProvider client={queryClient}>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" exact element={<Home/>}/>
+                  
+                </Routes>
+              </BrowserRouter>
+            {/* <ReactQueryDevtools/> */}
+            </QueryClientProvider> 
+          </ChakraProvider>
+        );
+      }
 
-      <Route path='/' exact>
-        <Home/>
-      </Route>
-    </Switch>
+      export default App;
 
-        </Router>
-        </QueryClientProvider> 
-        </ChakraProvider>
-      );
-    }
-
-    export default App;
-
-    // cd6807e42e3e13a8d56a28acd067d23e76d2b2b254f449e970cebf0406c09a13
+      // cd6807e42e3e13a8d56a28acd067d23e76d2b2b254f449e970cebf0406c09a13
