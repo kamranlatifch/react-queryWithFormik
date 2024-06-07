b863cace42a5273bec5834e52eda9e9e7603dac0
kamranlatifch <kamranlatif98@gmail.com>
2024-06-07 14:58:14 +0500

    add home and routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import { ChakraProvider, Heading } from '@chakra-ui/react';
-import './App.css';
-import { QueryClient,QueryClientProvider } from 'react-query';
-import Home from './Home/Home';
-function App() {
+    import { ChakraProvider, Heading, Switch } from '@chakra-ui/react';
+    import './App.css';
+    import { QueryClient,QueryClientProvider } from 'react-query';
+    import Home from './Home/Home';
+    import { Route, Router, Routes } from 'react-router-dom';
+    function App() {
 
-  const queryClient=new QueryClient();
-  return (
-    <ChakraProvider>
-    <QueryClientProvider client={queryClient}>
-    <Heading> Welcome To React Query Practice with with Formik and Yup
-    </Heading>
-    <Home/>
-    </QueryClientProvider> 
-    </ChakraProvider>
-  );
-}
+      const queryClient=new QueryClient();
+      return (
+        <ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+        {/* <Heading> Welcome To React Query Practice with with Formik and Yup
+        </Heading>
+        <Home/> */}
+        <Router>
+    <Switch>
 
-export default App;
+      <Route path='/' exact>
+        <Home/>
+      </Route>
+    </Switch>
 
-// cd6807e42e3e13a8d56a28acd067d23e76d2b2b254f449e970cebf0406c09a13
+        </Router>
+        </QueryClientProvider> 
+        </ChakraProvider>
+      );
+    }
+
+    export default App;
+
+    // cd6807e42e3e13a8d56a28acd067d23e76d2b2b254f449e970cebf0406c09a13
