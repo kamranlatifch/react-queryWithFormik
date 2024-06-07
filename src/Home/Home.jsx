f17b0eb494e87495b7c18f3f1116f45adf322c44
kamranlatifch <kamranlatif98@gmail.com>
2024-06-07 15:23:04 +0500

    add list mapping in home component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,27 +1,47 @@
-import axios from "axios";
-import React from "react";
-import { useQuery } from "react-query";
+    import { Container, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+    import axios from "axios";
+    import React from "react";
+    import { useQuery } from "react-query";
 
-const fetchPosts=async ()=>{
-    try{
- const {data}=await axios.get('https://gorest.co.in/public/v1/posts');   
+    const fetchPosts=async ()=>{
+        try{
+    const {data}=await axios.get('https://gorest.co.in/public/v1/posts');   
 
-return data;
-}catch (error){
-    throw Error("Unable to fetch posts.")
-}
-
-
-}
+    return data;
+    }catch (error){
+        throw Error("Unable to fetch posts.")
+    }
 
 
-const Home=()=>{
-const {data:PostsData, isLoading:PostsLoading}=useQuery('posts',fetchPosts);
-    
-    console.log("Posts Data is",PostsData)
-    return <div>
-        Home Page
-    </div>
-}
+    }
 
-export default Home;
+
+    const Home=()=>{
+    const {data:PostsData, isLoading:PostsLoading}=useQuery('posts',fetchPosts);
+        
+        console.log("Posts Data is",PostsData)
+        return (
+        <Container maxW="1300px" mt="4">
+            {PostsData?.data?.map((post)=>(
+            <Stack p="4" boxShadow="md" borderRadius="xl" border="1px solid #ccc">
+
+            <Flex justify="space-between">
+                <Text>
+                    UserId:
+                </Text>
+                <Text>
+                    PostId:
+                </Text>
+            </Flex>
+            <Heading fontSize="2xl">
+                Title
+            </Heading>
+            <Text>COntent</Text>
+            </Stack>
+            ))}
+           
+        </Container>
+        )
+    }
+
+    export default Home;
